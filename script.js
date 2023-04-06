78f30b9a81b415ce2bfc42c51d0005d28a6e0b37
Piotr Cholewinski <piotrcholewinski@icloud.com>
2023-04-06 16:11:20 +0200

    create 16x16 grid of square divs

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 // creating square divs
 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 256; i++) {
     const div = document.createElement("div");
-    div.style.width = "100px";
-    div.style.height = "100px";
-    div.style.backgroundColor = "blue";
+    div.style.width = "25px";
+    div.style.height = "25px";
+    div.style.backgroundColor = "white";
+    div.style.border = "solid black";
+    div.style.borderWidth = "0.6px";
+    div.style.boxSizing = "border-box";
     document.getElementById("grid").appendChild(div);
     console.log(div)
 }
